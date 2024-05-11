6bc5c8e593dc6b2a3677a4dc72c5a4cce6119549
munks <54328090+munks@users.noreply.github.com>
2024-05-12 03:23:10 +0900

    File System Fix

--- include/thread.hpp
+++ include/thread.hpp
@@ -3,6 +3,8 @@
 typedef struct PTDATA PTDATA, *LPPTDATA;
 
 typedef struct PTDATA {
+	char name[260];
+	char winname[260];
 	ULONG pid;
 	HANDLE thread;
 	HANDLE process;
@@ -10,6 +12,6 @@ typedef struct PTDATA {
 	LPPTDATA next;
 } PTDATA, *LPPTDATA;
 
-void Thread_CreateThread (ULONG);
+void Thread_CreateThread (HWND, LPCWSTR);
 void Thread_Init ();
 void Thread_Close ();
